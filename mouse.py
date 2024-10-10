934678f7483d9a2b7d46705c7d66bf49ce2903e3
Shy2593666979 <2593666979@qq.com>
2024-10-10 17:18:57 +0800

    Create mouse.py

---
+++ mouse.py
@@ -0,0 +1,14 @@
+from pynput import mouse
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        if button == mouse.Button.left:
+            print(f'鼠标左键点击: ({x}, {y})')
+        if button == mouse.Button.right:
+            print(f'鼠标右键点击: ({x}, {y})')
+
+def mouse_coordinate():
+
+    # 创建一个鼠标监听器
+    with mouse.Listener(on_click=on_click) as listener:
+        listener.join()
